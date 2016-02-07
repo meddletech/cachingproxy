f09c3a2b98662e3ecd22702d9364c4676dfa5ead
Ed Sykes <ed.sykes@gmail.com>
2016-02-07 14:02:39 +0000

    added cache-control and response headers

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var
     acceptor = http.createServer().listen(port),
     pass = require('stream').PassThrough,
     redis = require('redis').createClient(),
-    utils = require('./utils');   
+    utils = require('./utils'),
+    cacheControl = require('./cacheControl.js'),
+    q = require('q');   
 
 var targetOrigin = process.env.TARGETORIGIN || 'http://localhost';
 var target = utils.parseTargetOrigin(targetOrigin);
@@ -17,82 +19,125 @@ logger.info('target origin', target);
 var cacheLength = process.env.CACHELENGTH || 30 * 60; // 30 minutes
 logger.info('cahce length', cacheLength);
 
-var proxy = function(request, response){
-    var options = url.parse(targetOrigin + request.url);
-    options.headers = request.headers;
-    options.method = request.method;
-    options.headers.host = target.host;
+var serveFromTarget = function(request, response){
+    return q.Promise(function(resolve, reject){
+        var options = url.parse(targetOrigin + request.url);
+        options.headers = request.headers;
+        options.method = request.method;
+        options.headers.host = target.host;
 
-    var caching = new pass();
-    var redisCachedResponse = [];
-    var totalLength = 0;
-    caching.on('data', function(chunk){
-        redisCachedResponse.push(chunk);
-        totalLength += chunk.length;
+        var caching = new pass();
+        var redisCachedResponse = [];
+        var totalLength = 0;
+        caching.on('data', function(chunk){
+            redisCachedResponse.push(chunk);
+            totalLength += chunk.length;
+        });
+
+        var redisObject = {};
+
+        caching.on('end', function(){
+            redisObject.response = Buffer.concat(redisCachedResponse, totalLength);
+            var expire = 30 * 60; // 5 minutes
+
+            redis.set(request.redisKey, JSON.stringify(redisObject), 'NX', 'EX', expire, function(error){
+                if(error){
+                    reject(error);
+                }
+                else{
+                    resolve();
+                }
+            });
+        });
+
+        var connector = http.request(options, function(serverResponse) {
+            if(response['x-proxy-cache-cleared']){
+                serverResponse.headers['x-proxy-cache-cleared'] = response['x-proxy-cache-cleared'];
+            }
+
+            response.writeHeader(serverResponse.statusCode, serverResponse.headers);
+            redisObject.statusCode = serverResponse.statusCode;
+            redisObject.headers = serverResponse.headers;
+            serverResponse.pipe(caching);
+            serverResponse.pipe(response);
+        });
+
+        var bodyStream = new pass();
+        request.bodyChunks.forEach(function(chunk){
+            bodyStream.write(chunk);
+        });
+
+        bodyStream.end();
+
+        bodyStream.pipe(connector);   
     });
-
-    var redisObject = {};
-
-    caching.on('end', function(){
-        redisObject.response = Buffer.concat(redisCachedResponse, totalLength);
-        var expire = 5 * 60; // 5 minutes
-
-        redis.set(JSON.stringify(request.redisKey), JSON.stringify(redisObject), 'NX', 'EX', expire);
-    });
-
-    var connector = http.request(options, function(serverResponse) {
-        response.writeHeader(serverResponse.statusCode, serverResponse.headers);
-        redisObject.statusCode = serverResponse.statusCode;
-        redisObject.headers = serverResponse.headers;
-        serverResponse.pipe(caching);
-        serverResponse.pipe(response);
-    });
-
-    var bodyStream = new pass();
-    request.bodyChunks.forEach(function(chunk){
-        bodyStream.write(chunk);
-    });
-
-    bodyStream.end();
-
-    bodyStream.pipe(connector);   
 };
 
-acceptor.on('request', function(request, response) {
+var handleError = function(error){
+    console.log('error serving request: ', error);
+};
 
-    var requestReader = new pass();
-    var chunks = [];
-    var chunksLength = 0;
-    requestReader.on('data', function(chunk){
-        chunks.push(chunk);
-        chunksLength += chunk.length;
-    });
-
-    requestReader.on('end', function(){
-        request.bodyChunks = chunks;
-
-        var body = Buffer.concat(chunks, chunksLength);
-        
-        var redisKey = {
-            url: request.url,
-            body: body,
-            method: request.method
-        };
-
-        request.redisKey = redisKey;
-
-        redis.get(JSON.stringify(redisKey), function(error, redisResponse) {
-        if(redisResponse){
+var ProxyRequest = function(request, response, redis){
+    var Serve = function(redisKey, resolve, reject){
+        var serveFromRedis = function(redisResponse){
             var responseObject = JSON.parse(redisResponse);
+            
+            responseObject.headers['x-proxy-cache'] = true;
             response.writeHeader(responseObject.statusCode, responseObject.headers);
             response.write(new Buffer(responseObject.response.data));
             response.end();
-        }
-        else{
-                proxy(request, response);
-            }
-        });
-    });
+            resolve();
+        };
 
-    request.pipe(requestReader);
+        this.handle = function(){
+            redis.get(redisKey, function(error, redisResponse) {
+                if(error){
+                    reject(error);
+                }
+                else if(redisResponse){
+                    return serveFromRedis(redisResponse);
+                }
+                else{
+                    return serveFromTarget(request, response);
+                }
+            });
+        };
+    };
+
+    this.handle = function(){
+        return q.Promise(function(resolve, reject){
+            var requestReader = new pass();
+            var chunks = [];
+            var chunksLength = 0;
+            requestReader.on('data', function(chunk){
+                chunks.push(chunk);
+                chunksLength += chunk.length;
+            });
+
+            requestReader.on('end', function(){
+                request.bodyChunks = chunks;
+
+                var body = Buffer.concat(chunks, chunksLength);
+                
+                var redisKey = {
+                    url: request.url,
+                    body: body,
+                    method: request.method
+                };
+
+                request.redisKey = JSON.stringify(redisKey);
+
+                cacheControl.handleInvalidation(request, redis, response)
+                .then(new Serve(request.redisKey, resolve, reject).handle)
+                .catch(handleError)
+                .done();
+            });
+
+            request.pipe(requestReader);
+        });
+    };
+};
+
+acceptor.on('request', function(request, response) {
+    new ProxyRequest(request, response, redis).handle();
 });
